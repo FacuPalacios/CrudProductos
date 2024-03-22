cc3a212b6eeb82e1e0513a72106a097966a073d3
Facundo <facupalacios21@gmail.com>
2024-03-22 20:31:13 -0300

    se creo la estructura del sitio y se agrego react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-
+import NavBar from './components/NavBar.jsx'
+import Foot from './components/Foot.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "./components/pages/Home.jsx"
+import AcercaDeNosotros from './components/pages/AcercaDeNosotros.jsx'
+import Administracion from './components/pages/Administracion.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="bg-dark">
-        <h1>Hola mundo</h1>
-        </div>
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} /> {/* /: Cuando estoy en el inicio (google.com/), muestra el <Home/> */}
+            <Route path="/acercadenosotros" element={<AcercaDeNosotros />} />
+            <Route path="/administracion" element={<Administracion />} />
+          </Routes>
+        </main>
+        <footer>
+          <Foot />
+        </footer>
+      </BrowserRouter>
     </>
   )
 }
