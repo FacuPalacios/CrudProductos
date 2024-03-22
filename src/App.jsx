87eaf29609505a02f1d46dade4be8ec653bc0cb6
Facundo <facupalacios21@gmail.com>
2024-03-22 18:29:04 -0300

    se realizo la configuracion inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-dark">
+        <h1>Hola mundo</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
